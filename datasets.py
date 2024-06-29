d42f2c9e1269a54da09002ebdcdbf1ec2d16e30e
Egor Gladilin <49620289+REDISKA3000@users.noreply.github.com>
2024-06-29 14:50:16 +0300

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,25 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+import torch.nn.functional as F
+
+class TSDataset(Dataset):
+    def __init__(self, dataframe, target, features, sequence_length=5):
+        self.features = features
+        self.target = target
+        self.sequence_length = sequence_length
+        self.X = torch.tensor(dataframe[features].values).float()
+        self.y = torch.tensor(dataframe[target].values).float()
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, i):
+        if i >= self.sequence_length - 1:
+            i_start = i - self.sequence_length + 1
+            x = self.X[i_start:(i + 1), :]
+        else:
+            padding = self.X[0].repeat(self.sequence_length - i - 1, 1)
+            x = self.X[0:(i + 1), :]
+            x = torch.cat((padding, x), 0)
+
+        return x, self.y[i]
